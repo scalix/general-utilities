75e7e3161bc1afe4021a4b011b63b425dcc6f964
Bobyr Alexey <pussbb@gmail.com>
2015-03-05 15:17:50 +0200

    live_connection_monitor output fixes

--- live_connection_monitor/live_connection_monitor.sh
+++ live_connection_monitor/live_connection_monitor.sh
@@ -5,12 +5,12 @@ echo -e "Date\t\t\tTotal\tHttpd(CLOSE_WAIT)\tJava 8009(CLOSE_WAIT)\tJava 8080(CL
 while :
 do
     output=$(netstat -apn | grep ':80')
-    total=$(echo -n "$output" | grep ':80' | wc -l)
+    total=$(echo -n "$output" | wc -l)
     httpd_close_wait=$(echo -n "$output"| grep CLOSE | grep 'httpd\|apache2' | wc -l)
     java_close_wait=$(echo -n "$output" | grep CLOSE | grep 'java')
     java_close_wait_8009=$(echo -n "$java_close_wait" |grep 8009 | wc -l)
     java_close_wait_8080=$(echo -n "$java_close_wait" |grep 8080 | wc -l)
-    echo -ne "$(date '+%F %R:%S')\t$total\t\t$httpd_close_wait\t\t\t$java_close_wait_8009\t\t\t$java_close_wait_8080\t\t"\\r
+    echo -ne "$(date '+%F %R:%S')\t$total   \t\t$httpd_close_wait   \t\t\t$java_close_wait_8009   \t\t\t$java_close_wait_8080   \t\t"\\r
     sleep 1
 done
  
